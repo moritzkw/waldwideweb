98d78a8156f59ed2c68ad0e449fc4d2cfabb6e8b
moritzkw <55577255+moritzkw@users.noreply.github.com>
2023-05-09 19:50:10 +0200

    add visitor dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <v-theme-provider :theme="theme" with-background class="pa-10">
+    <v-app>
+      <v-main>
+        <v-app-bar color="green-darken-3">
+          <v-toolbar-title> WaldWideWeb </v-toolbar-title>
+          <template v-slot:append>
+            <v-btn icon="mdi-login"></v-btn>
+            <v-btn :icon="themeIcon" @click="toggleTheme"></v-btn>
+          </template>
+        </v-app-bar>
+        <VisitorDashboard />
+      </v-main>
+    </v-app>
+  </v-theme-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import VisitorDashboard from "./components/VisitorDashboard.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    VisitorDashboard,
   },
 
-  data () {
+  data() {
     return {
-      //
-}
+      theme: "light",
+      themeIcon: "mdi-brightness-2",
+    };
   },
-})
+
+  methods: {
+    toggleTheme() {
+      (this.theme = this.theme === "light" ? "dark" : "light"),
+        (this.themeIcon =
+          this.theme === "light" ? "mdi-brightness-2" : "mdi-brightness-5");
+    },
+  },
+});
 </script>
---
+++ src/components/VisitorDashboard.vue
@@ -0,0 +1,89 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Weather from "./Weather.vue";
+
+// Logo
+
+export default defineComponent({
+  components: { Weather },
+  name: "VisitorDashboard",
+
+  data() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card elevation="5">
+          <v-container fluid>
+            <v-row justify="space-between">
+              <v-col>
+                <v-card title="Temperatur" elevation="0">
+                  <div class="d-flex align-center">
+                    <v-icon
+                      icon="mdi-white-balance-sunny"
+                      color="yellow"
+                      size="x-large"
+                    />
+                    <div class="text-h2 ml-4">17°C</div>
+                  </div>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card title="Luftfeuchtigkeit" elevation="0">
+                  <div class="d-flex align-center">
+                    <v-icon
+                      icon="mdi-water-outline"
+                      color="blue"
+                      size="x-large"
+                    />
+                    <div class="text-h2 ml-4">34%</div>
+                  </div>
+                </v-card>
+              </v-col>
+              <v-col>
+                <v-card title="Wind" elevation="0">
+                  <div class="d-flex align-center">
+                    <v-icon
+                      icon="mdi-weather-windy"
+                      color="grey"
+                      size="x-large"
+                    />
+                    <div class="text-h2 ml-4">8km/h</div>
+                  </div>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card title="Besucherzahl" elevation="5">
+          <v-container>
+            <v-row>
+              <v-col>
+                <div class="d-flex align-center">
+                  <div class="text-h2">187</div>
+                  <v-icon icon="mdi-arrow-top-right ml-4" color="green" />
+                </div>
+              </v-col>
+              <v-col> 2 </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+      <v-col>
+        <!-- <v-card title="B" elevation="5">
+                        test
+                    </v-card> -->
+        <weather></weather>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>

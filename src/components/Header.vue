c2b622c6aef2ac1745a68bc836dfe67e41fc2e5b
moritzkw <55577255+moritzkw@users.noreply.github.com>
2023-05-09 17:09:08 +0200

    init vuetify

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+    }
+  }
+})
+</script>
+
+<template>
+    <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-button></v-button>
+        Test
+    </v-app-bar>
+</template>

625b578818fb6dece4c1363212efa636a64d14ef
moritzkw <55577255+moritzkw@users.noreply.github.com>
2023-06-05 15:32:38 +0200

    change title and add file upload error

--- src/components/AdminDashboard.vue
+++ src/components/AdminDashboard.vue
@@ -13,7 +13,9 @@ export default defineComponent({
   name: "AdminDashboard",
 
   data() {
-    return {};
+    return {
+      error: false
+    };
   },
   mounted() {
     console.debug(this.$store.state.admin.users);
@@ -41,29 +43,36 @@ export default defineComponent({
 <template>
   <v-container fluid>
     <v-col>
-      <v-card class="mb-4 pa-4" elevation="5" title="Sensor Updates (aktuell: v1.0.1)">
+      <v-card class="mb-4 pa-4" elevation="5" title="Sensor Updates" >
         <v-col>
+          <v-row>
+            <div class="ml-4">Aktuelle Version: 0.0.1</div>
+          </v-row>
           <v-row class="mb-8" v-if="true">
             <v-alert
-              text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd."
+              class="mt-2"
+              v-if="error"
+              text="Es ist ein Fehler während des Uploads der neuen Softwareversion aufgetreten!"
               icon="mdi-alert-circle-outline"
               color="error"
             ></v-alert>
           </v-row>
           <v-row>
             <v-file-input label="Binary auswählen" accept=".bin" class="fileinput mr-8"></v-file-input>
-            <v-btn append-icon="mdi-upload" class="mt-2">Hochladen</v-btn>
+            <v-btn append-icon="mdi-upload" class="mt-2" @click="error=true">Hochladen</v-btn>
           </v-row>
         </v-col>
       </v-card>
       <v-card class="pa-4" elevation="5">
-        <v-btn append-icon="mdi-plus">Nutzer anlegen</v-btn>
+        <v-btn append-icon="mdi-plus" class="mr-4 my-2">Nutzer anlegen</v-btn>
+        <v-btn append-icon="mdi-refresh" class="my-2">Aktualisieren</v-btn>
         <v-list class="mt-4">
           <v-container class="pa-0" v-bind="user" v-for="user in users">
             <v-list-item :key="user.email" :title="user.email">
               <template v-slot:append>
                 <EditUserDialog :user="user"/>
-                <DeleteUserDialog :user="user"/>
+                <v-spacer></v-spacer>
+                <DeleteUserDialog :user="user" class="ml-4"/>
               </template>
             </v-list-item>
             <v-divider></v-divider>

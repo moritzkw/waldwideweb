69c0a8b2455534ccfbb3812f418645a09a5ddf4b
eeisemann220 <Eisemann16@gmail.com>
2023-06-23 01:06:34 +0200

    add: all markers to map

--- src/components/ForesterDashboard.vue
+++ src/components/ForesterDashboard.vue
@@ -3,12 +3,10 @@ import { defineComponent } from "vue";
 import Weather from "./Weather.vue";
 import Temperature from "./Temperature.vue";
 import { GChart } from "vue-google-charts";
-import { GoogleMap, Marker } from "vue3-google-map";
-
-// Logo
+import { GoogleMap, Marker} from "vue3-google-map";
 
 export default defineComponent({
-  components: { Weather, GChart, Temperature, GoogleMap, Marker },
+  components: { Weather, GChart, Temperature, GoogleMap, Marker},
   name: "ForesterDashboard",
 
   data() {
@@ -48,14 +46,75 @@ export default defineComponent({
         },
         // curveType: 'function'
       },
+      hoveredNode: null as any,
+      infoWindowPosition: null as null | { lat: number; lng: number; },
+      infoWindowOptions: {},
+      infoWindowNode: null as null | { [key: string]: any },
+      mapCenter: null,
+      mapZoom: null,
     };
+  },
+
+  methods: {
+
+    openInfoWindow(node: any) {
+      this.infoWindowNode = node;
+      // Set the position and options of the info window based on the marker's position
+      this.infoWindowPosition = { lat: node.latitude, lng: node.longitude };
+      this.infoWindowOptions = { /* customize the options if needed */ };
+    },
+
+  getMarkerOptions(node: any) {
+    const latitude = parseFloat(node.latitude);
+    const longitude = parseFloat(node.longitude);
+
+    // Überprüfe, ob die Werte numerisch sind
+    if (isNaN(latitude) || isNaN(longitude)) {
+      console.error('Ungültige Längen- oder Breitengrade:', node.latitude, node.longitude);
+      return null;
+    }
+
+    // Erstelle die Optionen für den Marker
+    const markerOptions = {
+      position: { lat: latitude, lng: longitude },
+      isHovered: this.isNodeHovered(node),
+    };
+
+    return markerOptions;
+  },
+
+  isNodeHovered(node: any) {
+  return this.hoveredNode !== null && this.hoveredNode !== undefined && this.hoveredNode.uuid === node.uuid;
+},
+
+  setHoveredNode(node: any) {
+      this.hoveredNode = node;
+    },
+    clearHoveredNode() {
+      this.hoveredNode = null;
+    },
+    handleNodeClick(node: any) {
+    // Führe hier die gewünschte Logik aus, wenn auf ein Node geklickt wird
+    console.log('Node clicked:', node);
+  },
+
+  handleNodeHover(node: any) {
+    // Setze den gehoverten Node im Zustand der Komponente
+    this.hoveredNode = node;
+  },
+},
+
+  mounted() {
+      console.debug(this.store.state
+      )
   },
   computed: {
     store() {
       return this.$store;
     },
   },
-});
+  },
+);
 </script>
 
 <template>
@@ -105,9 +164,31 @@ export default defineComponent({
       <v-col>
         <v-card class="card" title="Sensoren" :elevation="5">
           <v-container>
-            <GoogleMap api-key="AIzaSyBiaS391syegtj4i98-M0E7ylzmItDTDsc" style="height: 300px" :center="center" :zoom="15">
+            <GoogleMap api-key="AIzaSyBiaS391syegtj4i98-M0E7ylzmItDTDsc" 
+              style="height: 500px" 
+              :center="center"
+              :zoom="15">
               <Marker :options="{ position: center }" />
-              <Marker :options="{ position: center }" />
+              <!-- Iteriere über die Nodes und erstelle Marker -->
+              <Marker v-for="node in store.state.nodes" 
+                :key="node.id" 
+                :options="getMarkerOptions(node)" 
+                @click="handleNodeClick(node)"
+                @mouseover="handleNodeHover(node)"
+                @mouseout="clearHoveredNode" 
+              />
+              <info-window
+                v-if="infoWindowNode !== null"
+                :position="infoWindowPosition"
+                :options="infoWindowOptions"
+              >
+             <div v-if="isNodeHovered(hoveredNode)" class="node-overlay">
+               <p>CreatedAt: {{ hoveredNode.createdAt }}</p>
+               <p>Latitude: {{ hoveredNode.latitude }}</p>
+               <p>Longitude: {{ hoveredNode.longitude }}</p>
+               <p>UUID: {{ hoveredNode.uuid }}</p>
+              </div>
+              </info-window>
             </GoogleMap>
           </v-container>
         </v-card>
@@ -159,4 +240,13 @@ export default defineComponent({
 .card:hover {
   cursor: pointer;
 }
+.node-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px;
+    background-color: rgba(255, 255, 255, 0.9);
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+    z-index: 9999;
+  }
 </style>
